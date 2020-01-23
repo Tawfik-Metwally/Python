c01a8542bfa1aa35da9258e699788f7de4c93e94
Tawfik Metwally <Tawfikaly02@gmail.com>
2020-01-23 06:11:02 -0300

    Adicionando novo arquivo

---
+++ 5-Dígito das dezenas.py
@@ -0,0 +1,5 @@
+Numero = input("Digite um número inteiro: ")
+N = float(int(Numero))
+x = N // 10
+y = x % 10
+print("O dígito das dezenas é",y)
---
+++ 6-Par ou Ímpar.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número inteiro: "))
+Numero = numero % 10
+N = Numero % 2
+if N == 0 :
+    print("par")
+else:
+    print("ímpar")
